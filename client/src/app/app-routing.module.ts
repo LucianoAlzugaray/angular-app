9f11f490aee962d9f5241a21a46a29d75d65170f
Luciano Alzugaray <lucianoalzugaray1@gmail.com>
2018-08-26 03:51:00 -0300

    routing con admin agregado

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent }       from './login/login.component';
 import { InsurancesComponent }  from './insurances/insurances.component';
 import { UsersComponent }       from './users/users.component';
+import { AdminComponent }       from './admin/admin.component';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent },
-  {path: 'insurances', component: InsurancesComponent },
-  {path: 'users', component: UsersComponent },
-];
+  {    
+    path: '',
+    component: AdminComponent,
+    children: [
+      { path: '', redirectTo: 'users', pathMatch: 'full' },
+      {path: 'insurances', component: InsurancesComponent },
+      {path: 'users', component: UsersComponent },
+    ]
+  }, 
+  { path: 'login', component: LoginComponent },
+ ];
 
 @NgModule({
   imports: [
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
